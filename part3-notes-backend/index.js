ee0a9afb982f2dbf2d04c2add70fe260d171e963
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2025-04-02 16:56:08 -0700

    add part3-note-backend from course

--- part3-notes-backend/index.js
+++ part3-notes-backend/index.js
@@ -1,45 +1,36 @@
 require("dotenv").config();
 const express = require("express");
-//const cors = require("cors");
-const mongoose = require("mongoose");
 const Note = require("./models/note");
 
 const app = express();
 
-app.use(express.json());
-//app.use(cors());
+let notes = [];
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 app.use(express.static("dist"));
-
-const password = process.argv[2];
-const url = `mongodb+srv://brandonwilletts:${password}@cluster0.qt70dcd.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
   Note.findById(request.params.id).then((note) => {
     response.json(note);
-  });
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
   });
 });
 
@@ -59,6 +50,19 @@ app.post("/api/notes", (request, response) => {
     response.json(savedNote);
   });
 });
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
