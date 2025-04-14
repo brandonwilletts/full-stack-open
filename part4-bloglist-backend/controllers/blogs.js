529a1ee1180950e3455c70d1d2e51a13f7020263
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2025-04-13 19:17:27 -0700

    part4 complete + small fixes

--- part4-bloglist-backend/controllers/blogs.js
+++ part4-bloglist-backend/controllers/blogs.js
@@ -1,53 +1,51 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const middleware = require('../utils/middleware');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user');
-  response.json(blogs);
+	const blogs = await Blog.find({}).populate('user');
+	response.json(blogs);
 });
 
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id);
-  if (blog) {
-    response.json(blog);
-  } else {
-    response.status(404).end();
-  }
+	const blog = await Blog.findById(request.params.id);
+	if (blog) {
+		response.json(blog);
+	} else {
+		response.status(404).end();
+	}
 });
 
 blogsRouter.post('/', middleware.userExtractor, async (request, response,) => {
-  const user = request.user;
-  const blog = new Blog(request.body);
-  blog.user = user._id;
-  const savedBlog = await blog.save();
-  response.status(201).json(savedBlog);
+	const user = request.user;
+	const blog = new Blog(request.body);
+	blog.user = user._id;
+	const savedBlog = await blog.save();
+	response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
-  const user = request.user;
-  const blog = await Blog.findById(request.params.id);
-  if (blog.user.toString() !== user._id.toString()) {
-    return response.status(401).json({ error: 'invalid user' });
-  }
+	const user = request.user;
+	const blog = await Blog.findById(request.params.id);
+	if (blog.user.toString() !== user._id.toString()) {
+		return response.status(401).json({ error: 'invalid user' });
+	}
 
-  await Blog.deleteOne(blog);
-  response.status(204).end();
+	await Blog.deleteOne(blog);
+	response.status(204).end();
 });
 
 blogsRouter.put('/:id', async (request, response) => {
-  const blog = {
-    id: request.body.id,
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes,
-    user: request.body.user
-  };
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
-  response.json(updatedBlog);
+	const blog = {
+		id: request.body.id,
+		title: request.body.title,
+		author: request.body.author,
+		url: request.body.url,
+		likes: request.body.likes,
+		user: request.body.user
+	};
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+	response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
